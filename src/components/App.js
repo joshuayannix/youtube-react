4ec555710011fb4ba581373921579ed931bc7b10
Joshua Yannix <joshua.yannix@topbloc.com>
2020-07-19 15:01:15 -0500

    created most components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import SearchBar from './SearchBar'
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+
+const KEY = 'AIzaSyB6Sx1NOW_vLYe-9d-zl8R2Lp6I9NhpUzM';
 
 class App extends React.Component {
+  state = { videos: [] };
+
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+        part: 'snippet',
+        type: 'video',
+        maxResults: '10',
+        key: KEY
+      }
+    })
+    this.setState({ videos: response.data.items })
+  }
+
   render() {
     return (
       <div className='ui container'>
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     )
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,13 +2,16 @@ import React from 'react'
 
 class SearchBar extends React.Component {
   state = { term: '' };
+
   onInputChange = (event) => {
     this.setState({ term: event.target.value })
   }
+
   onFormSubmit = event => {
     event.preventDefault();
-    
+    this.props.onFormSubmit(this.state.term)
   }
+  
   render() {
     return (
       <div className='search-bar ui segment'>
